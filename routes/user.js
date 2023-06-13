309da32dd4819a6cf854846e4bfb261875f7c6ce
jieun <najee0610@naver.com>
2023-06-14 01:50:31 +0900

    Feat: 게시글 삭제 front js코드, 언팔로우 기능 추가

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 const express = require("express");
 
 const { isLoggedIn } = require("./middlewares");
+const User = require("../models/user");
 
 const router = express.Router();
 
-// POST /user/:id/follow
+// 팔로우 기능
 router.post("/:id/follow", isLoggedIn, async (req, res, next) => {
   try {
     // 현재 로그인한 사용자 정보 조회
@@ -16,8 +17,23 @@ router.post("/:id/follow", isLoggedIn, async (req, res, next) => {
       res.send("success");
     } else {
       // 사용자를 찾을 수 없는 경우
-      res.status(404).send("no user");
+      res.status(404).send("사용자를 찾을 수 없습니다.");
     }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// 언팔로우 기능
+router.delete("/:userId/follow", isLoggedIn, async (req, res, next) => {
+  try {
+    const user = await User.findOne({ where: { id: req.params.userId } });
+    if (!user) {
+      res.status(403).send("언팔로우가 불가능합니다!");
+    }
+    await user.removeFollowers(req.user.id);
+    res.status(200).json({ UserId: parseInt(req.params.userId, 10) });
   } catch (error) {
     console.error(error);
     next(error);
